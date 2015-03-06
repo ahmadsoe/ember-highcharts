a4adec483aa4fd1e5c7875365ec5cbd1a09f41f8
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-03-06 01:58:35 -0800

    Updated ember-cli to 0.1.15

--- tests/unit/components/high-charts-test.js
+++ tests/unit/components/high-charts-test.js
@@ -2,36 +2,51 @@ import {
   moduleForComponent,
   test
 } from 'ember-qunit';
-import { initialize } from '../../../initializers/highcharts';
+import { initialize } from '../../../app/initializers/highcharts';
 
 moduleForComponent('high-charts', 'HighChartsComponent', {
   // specify the other units that are required for this test
-  setup: function(container) {
+  needs: [ 'highcharts-config:application' ],
+  beforeEach: function(container) {
     initialize(container);
-  },
-  needs: ['highcharts-config:application']
+  }
 });
 
-test('it renders', function() {
-  expect(2);
+test('it renders', function(assert) {
+  assert.expect(2);
 
   // creates the component instance
   var component = this.subject();
-  equal(component._state, 'preRender');
+  assert.equal(component._state, 'preRender');
 
   // appends the component to the page
-  this.append();
-  equal(component._state, 'inDOM');
+  this.append(assert);
+  assert.equal(component._state, 'inDOM');
 });
 
-test('has default options', function() {
+test('has default options', function(assert) {
+  assert.expect(1);
+
   var element = this.append();
-  var credit = element.find(':contains(Highcharts.com)');
-  equal(credit.length, 0, 'Highcharts default credits not present');
+  var credit  = element.find(':contains(Highcharts.com)');
+
+  assert.equal(credit.length, 0, 'Highcharts default credits not present');
 });
 
-test('has local options', function() {
+test('has local options', function(assert) {
+  assert.expect(1);
+
   var element = this.append();
-  var credit = element.find(':contains(ember-highcharts-configured-title)');
-  notEqual(credit.length, 0, 'ember-highcharts-configured-title credits present');
+  var credit  = element.find(':contains(ember-highcharts-configured-title)');
+
+  assert.notEqual(credit.length, 0, 'ember-highcharts-configured-title credits present');
+});
+
+test('stores reference to chart', function(assert) {
+  assert.expect(1);
+
+  var component = this.subject();
+  var chart     = component.get('chart');
+
+  assert.ok(chart);
 });

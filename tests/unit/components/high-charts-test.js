527df5a96bbc12cb472eb151095b6029386baeea
Lauren Tan <lauren.elizabeth.tan@gmail.com>
2015-03-06 02:08:45 -0800

    Fixed tests

--- tests/unit/components/high-charts-test.js
+++ tests/unit/components/high-charts-test.js
@@ -2,12 +2,13 @@ import {
   moduleForComponent,
   test
 } from 'ember-qunit';
-import { initialize } from '../../../app/initializers/highcharts';
+import { initialize } from '../../../initializers/highcharts';
 
 moduleForComponent('high-charts', 'HighChartsComponent', {
   // specify the other units that are required for this test
   needs: [ 'highcharts-config:application' ],
-  beforeEach: function(container) {
+  beforeEach: function() {
+    let container = this.container;
     initialize(container);
   }
 });
@@ -16,19 +17,19 @@ test('it renders', function(assert) {
   assert.expect(2);
 
   // creates the component instance
-  var component = this.subject();
+  let component = this.subject();
   assert.equal(component._state, 'preRender');
 
   // appends the component to the page
-  this.append(assert);
+  this.render(assert);
   assert.equal(component._state, 'inDOM');
 });
 
 test('has default options', function(assert) {
   assert.expect(1);
 
-  var element = this.append();
-  var credit  = element.find(':contains(Highcharts.com)');
+  let element = this.render();
+  let credit  = element.find(':contains(Highcharts.com)');
 
   assert.equal(credit.length, 0, 'Highcharts default credits not present');
 });
@@ -36,17 +37,8 @@ test('has default options', function(assert) {
 test('has local options', function(assert) {
   assert.expect(1);
 
-  var element = this.append();
-  var credit  = element.find(':contains(ember-highcharts-configured-title)');
+  let element = this.render();
+  let credit  = element.find(':contains(ember-highcharts-configured-title)');
 
   assert.notEqual(credit.length, 0, 'ember-highcharts-configured-title credits present');
-});
-
-test('stores reference to chart', function(assert) {
-  assert.expect(1);
-
-  var component = this.subject();
-  var chart     = component.get('chart');
-
-  assert.ok(chart);
 });

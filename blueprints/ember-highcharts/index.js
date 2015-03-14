7abeeb332bed380b17a8655d2c32e6a1dcc10912
ahmadsoe <ahmadsoe@gmail.com>
2015-03-15 05:06:20 +0700

    the blueprint is no longer used

--- blueprints/ember-highcharts/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  normalizeEntityName: function() {},
-
-  afterInstall: function() {
-    // We expect the user to add the package he needs
-    //return this.addBowerPackageToProject('highcharts-release#~4.0.4');
-  }
-};

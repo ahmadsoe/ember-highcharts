79342c5770e2d2f49e6d441a7b20d7380329fcbb
ahmadsoe <ahmadsoe@gmail.com>
2015-08-20 10:35:56 +0700

    removed custom preprocessor

--- index.js
+++ index.js
@@ -4,20 +4,6 @@
 module.exports = {
   name: 'ember-highcharts',
 
-  setupPreprocessorRegistry: function(type, registry) {
-    var options = getOptions(this.parent && this.parent.options && this.parent.options['babel']);
-
-    var plugin = {
-      name   : 'ember-cli-babel',
-      ext    : 'js',
-      toTree : function(tree) {
-        return require('broccoli-babel-transpiler')(tree, options);
-      }
-    };
-
-    registry.add('js', plugin);
-  },
-
   included: function(app) {
     this._super.included(app);
 
@@ -44,27 +30,3 @@ module.exports = {
     }
   }
 };
-
-function getOptions(options) {
-  options = options || {};
-
-  // Ensure modules aren't compiled unless explicitly set to compile
-  options.blacklist = options.blacklist || ['es6.modules'];
-
-  if (options.compileModules === true) {
-    if (options.blacklist.indexOf('es6.modules') >= 0) {
-      options.blacklist.splice(options.blacklist.indexOf('es6.modules'), 1);
-    }
-
-    delete options.compileModules;
-  } else {
-    if (options.blacklist.indexOf('es6.modules') < 0) {
-      options.blacklist.push('es6.modules');
-    }
-  }
-
-  // Ember-CLI inserts its own 'use strict' directive
-  options.blacklist.push('useStrict');
-
-  return options;
-}

58e9b9c418968a9fa19f63c0c4a953db11c60619
Garrett Murphey <gmurphey@trovedata.com>
2016-01-28 10:42:01 -0500

    Moving bower dependencies to npm.

--- blueprints/ember-highcharts/index.js
+++ blueprints/ember-highcharts/index.js
@@ -4,6 +4,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('highcharts');
+    return this.addPackageToProject('highcharts');
   }
 };
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /* jshint node: true */
 'use strict';
 
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var path = require('path');
+
 module.exports = {
   name: 'ember-highcharts',
 
@@ -9,32 +13,48 @@ module.exports = {
 
     var app = target.app || target;
     var options = app.options.emberHighCharts || { includeHighCharts: true };
+    var highchartsPath = 'vendor/highcharts';
 
     if (options.includeHighCharts || options.includeHighCharts3D) {
-      app.import(app.bowerDirectory + '/highcharts/highcharts.src.js');
+      app.import(highchartsPath + '/highcharts.src.js');
     }
 
     if (options.includeHighStock) {
-      app.import(app.bowerDirectory + '/highcharts/highstock.src.js');
+      app.import(highchartsPath + '/highstock.src.js');
     }
 
     if (options.includeHighMaps) {
-      app.import(app.bowerDirectory + '/highcharts/highmaps.src.js');
+      app.import(highchartsPath + '/highmaps.src.js');
     }
 
     if (options.includeHighChartsMore) {
-      app.import(app.bowerDirectory + '/highcharts/highcharts-more.src.js');
+      app.import(highchartsPath + '/highcharts/highcharts-more.src.js');
     }
 
     if (options.includeHighCharts3D) {
-      app.import(app.bowerDirectory + '/highcharts/highcharts-3d.src.js');
+      app.import(highchartsPath + '/highcharts/highcharts-3d.src.js');
     }
 
     if (options.includeModules) {
       var modules = options.includeModules;
       for (var i = 0; i < modules.length; i++) {
-        app.import(app.bowerDirectory + '/highcharts/modules/' + modules[i] + '.src.js');
+        app.import(highchartsPath + '/modules/' + modules[i] + '.src.js');
       }
     }
+  },
+
+  treeForVendor: function(vendorTree) {
+    var trees = [];
+    var highchartsPath = path.dirname(require.resolve('highcharts'));
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(new Funnel(highchartsPath, {
+      destDir: 'highcharts'
+    }));
+
+    return mergeTrees(trees);
   }
 };

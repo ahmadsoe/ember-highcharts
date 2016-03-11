1adda7ea1105481e600976102d86aeb3b81efd2a
Greg Jopa <grjopa@gmail.com>
2016-03-11 14:50:53 -0800

    Clean up tests

--- tests/integration/components/high-charts-test.js
+++ tests/integration/components/high-charts-test.js
@@ -4,11 +4,26 @@ import hbs from 'htmlbars-inline-precompile';
 
 import {
   lineChartOptions,
-  cityData
+  stockChartOptions,
+  cityData,
+  stockData
 } from '../constants';
 
 moduleForComponent('high-charts', 'Integration | Component | High Charts', {
   integration: true
+});
+
+test('should include local options', function(assert) {
+  assert.expect(2);
+
+  this.render(hbs`
+    {{high-charts}}
+  `);
+
+  // custom highcharts-configs/application.js is auto-loaded from tests/dummy/app
+  assert.equal(this.$('text:contains(Highcharts.com)').length, 0, 'default credits not present');
+  assert.notEqual(this.$(':contains(ember-highcharts-configured-title)').length, 0,
+    'expected credits text present');
 });
 
 test('should add a series', function(assert) {
@@ -20,7 +35,7 @@ test('should add a series', function(assert) {
     {{high-charts content=cityData chartOptions=lineChartOptions}}
   `);
 
-  assert.equal($('.highcharts-legend .highcharts-legend-item').length, 3, 'incorrect series count');
+  assert.equal(this.$('.highcharts-legend .highcharts-legend-item').length, 3, 'base series count');
 
   // add a series to chart content
   let cityDataCopy = Ember.copy(cityData, true);
@@ -34,7 +49,7 @@ test('should add a series', function(assert) {
   });
 
   this.set('cityData', cityDataCopy);
-  assert.equal($('.highcharts-legend .highcharts-legend-item').length, 4, 'incorrect series count');
+  assert.equal(this.$('.highcharts-legend .highcharts-legend-item').length, 4, 'new series count');
 });
 
 test('should remove a series', function(assert) {
@@ -46,12 +61,25 @@ test('should remove a series', function(assert) {
     {{high-charts content=cityData chartOptions=lineChartOptions}}
   `);
 
-  assert.equal($('.highcharts-legend .highcharts-legend-item').length, 3, 'incorrect series count');
+  assert.equal(this.$('.highcharts-legend .highcharts-legend-item').length, 3, 'base series count');
 
   // remove a series from chart content
   let cityDataCopy = Ember.copy(cityData, true);
   cityDataCopy = cityDataCopy.slice(0, 2);
 
   this.set('cityData', cityDataCopy);
-  assert.equal($('.highcharts-legend .highcharts-legend-item').length, 2, 'incorrect series count');
+  assert.equal(this.$('.highcharts-legend .highcharts-legend-item').length, 2, 'new series count');
+});
+
+test('should have navigator series for highstock', function(assert) {
+  assert.expect(1);
+
+  this.stockData = stockData;
+  this.stockChartOptions = stockChartOptions;
+
+  this.render(hbs`
+    {{high-charts mode="StockChart" content=stockData chartOptions=stockChartOptions}}
+  `);
+
+  assert.equal(this.$('.highcharts-navigator').length, 1, '.highcharts-navigator class is present');
 });

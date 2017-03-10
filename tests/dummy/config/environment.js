359cea8619446b36ea660a2cabc4b907cb805d42
Ahmad Suhendri <ahmadsoe@gmail.com>
2017-03-10 10:32:00 +0700

    Upgrade to ember-cli 2.11

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -10,15 +10,16 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
       }
     },
 
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    },
-    contentSecurityPolicy: {
-      'style-src': "'self' 'unsafe-inline'"
     }
   };
 

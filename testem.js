c94a4085df2f15d2bb8d5b3e331d20dcbc833384
Daniel Hagman <dhagman@linkedin.com>
2020-06-10 10:48:46 -0700

    add back ember-lts-2.12 and 2.16 tests

--- testem.js
+++ testem.js
@@ -1,6 +1,12 @@
+/* eslint-env node */
+
+'use strict';
+
 module.exports = {
+  framework: 'qunit',
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
+  parallel: -1,
   launch_in_ci: [
     'Chrome'
   ],
@@ -9,16 +15,22 @@ module.exports = {
   ],
   browser_args: {
     Chrome: {
-      mode: 'ci',
-      args: [
-        // --no-sandbox is needed when running Chrome inside a container
-        process.env.TRAVIS ? '--no-sandbox' : null,
-
-        '--disable-gpu',
+      ci: [
+        process.env.TRAVIS ? '--no-sandbox' : null, // --no-sandbox is needed when running Chrome inside a container
         '--headless',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
         '--remote-debugging-port=0',
-        '--window-size=1440,900'
-      ].filter(Boolean)
+        '--window-size=1440,900',
+        '--enable-logging',
+        '--v=1',
+        '--enable-crash-reporting',
+        '--crash-dumps-dir=build/outputs/chrome_crash_dumps',
+        // Chrome 66+ introduces a policy that requires user interaction before autoplay is allowed
+        // this flag disables that policy and allows autoplay regardless of user interaction
+        '--autoplay-policy=no-user-gesture-required',
+      ].filter(Boolean),
     }
   }
 };

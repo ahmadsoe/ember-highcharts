377d3483116d943379707f63c6eaf7ff19c500dd
Daniel Hagman <dhagman@linkedin.com>
2020-06-12 11:30:31 -0700

    drop ember 2 support

--- config/ember-try.js
+++ config/ember-try.js
@@ -12,30 +12,6 @@ module.exports = function() {
       useYarn: true,
       scenarios: [
         {
-          name: 'ember-lts-2.12',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.12.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.16.0'
-            }
-          }
-        },
-        {
-          name: 'ember-lts-2.18',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.18.0'
-            }
-          }
-        },
-        {
           name: 'ember-lts-3.0',
           npm: {
             devDependencies: {
